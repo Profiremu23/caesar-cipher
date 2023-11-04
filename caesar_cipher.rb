d6d830976aac0e9624a57f43177576d5cef5ead6
Profiremu23 <profirdani23@gmail.com>
2023-11-04 19:36:14 +0100

    Added software execution method manually

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -3,3 +3,10 @@ def caesar_cipher(string, index)
   adder = decoder.map { |c| c + index }
   returner = adder.map { |c| c.chr }.join
 end
+
+p "Please enter a string to encrypt..."
+text = gets.chomp
+p "Please enter a number to shift the characters within the string..."
+idx_shift = gets.chomp.to_i
+
+caesar_cipher(text, idx_shift)
